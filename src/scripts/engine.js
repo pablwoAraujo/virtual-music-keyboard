4468d4367f66774e8b3c300a579d3f069b5fea8f
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2023-11-13 16:27:26 -0300

    feat: escondendo o texto das teclas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
 
 let audio = new Audio("src/tunes/a.wav");
 let mapedKeys = [];
@@ -31,4 +32,9 @@ const handleVolume = (e) => {
   audio.volume = e.target.value;
 };
 
+const showHideKeys = () => {
+  pianoKeys.forEach((key) => key.classList.toggle("hide"));
+};
+
 volumeSlider.addEventListener("input", handleVolume);
+keysCheck.addEventListener("click", showHideKeys);
